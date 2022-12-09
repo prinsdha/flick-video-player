38ace5b19f74106326531e6e370c38dc92c7d5aa
prins <prince.wowtalent@live>
2022-12-09 21:46:27 +0530

    gesture added

--- lib/src/controls/flick_portrait_controls.dart
+++ lib/src/controls/flick_portrait_controls.dart
@@ -28,25 +28,23 @@ class FlickPortraitControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: FlickShowControlsAction(
-              child: FlickSeekVideoAction(
-                child: Center(
-                  child: FlickVideoBuffer(
-                    child: FlickAutoHideChild(
-                      showIfVideoNotInitialized: false,
-                      child: FlickPlayToggle(
-                        size: 30,
-                        color: Colors.black,
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.white70,
-                          borderRadius: BorderRadius.circular(40),
-                        ),
+    return Stack(
+      children: <Widget>[
+        Positioned.fill(
+          child: FlickShowControlsAction(
+            handleVideoTap: onTap,
+            child: FlickSeekVideoAction(
+              child: Center(
+                child: FlickVideoBuffer(
+                  child: FlickAutoHideChild(
+                    showIfVideoNotInitialized: false,
+                    child: FlickPlayToggle(
+                      size: 30,
+                      color: Colors.black,
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(40),
                       ),
                     ),
                   ),
@@ -54,69 +52,69 @@ class FlickPortraitControls extends StatelessWidget {
               ),
             ),
           ),
-          Positioned.fill(
-            child: FlickAutoHideChild(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    FlickVideoProgressBar(
-                      flickProgressBarSettings: progressBarSettings,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        FlickPlayToggle(
-                          size: iconSize,
-                        ),
-                        SizedBox(
-                          width: iconSize / 2,
-                        ),
-                        FlickSoundToggle(
-                          size: iconSize,
-                        ),
-                        SizedBox(
-                          width: iconSize / 2,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            FlickCurrentPosition(
-                              fontSize: fontSize,
+        ),
+        Positioned.fill(
+          child: FlickAutoHideChild(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlickVideoProgressBar(
+                    flickProgressBarSettings: progressBarSettings,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      FlickPlayToggle(
+                        size: iconSize,
+                      ),
+                      SizedBox(
+                        width: iconSize / 2,
+                      ),
+                      FlickSoundToggle(
+                        size: iconSize,
+                      ),
+                      SizedBox(
+                        width: iconSize / 2,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          FlickCurrentPosition(
+                            fontSize: fontSize,
+                          ),
+                          FlickAutoHideChild(
+                            child: Text(
+                              ' / ',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: fontSize),
                             ),
-                            FlickAutoHideChild(
-                              child: Text(
-                                ' / ',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: fontSize),
-                              ),
-                            ),
-                            FlickTotalDuration(
-                              fontSize: fontSize,
-                            ),
-                          ],
-                        ),
-                        Expanded(
-                          child: Container(),
-                        ),
-                        FlickSubtitleToggle(
-                          size: iconSize,
-                        ),
-                        SizedBox(
-                          width: iconSize / 2,
-                        ),
-                        FlickFullScreenToggle(
-                          size: iconSize,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                          ),
+                          FlickTotalDuration(
+                            fontSize: fontSize,
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      FlickSubtitleToggle(
+                        size: iconSize,
+                      ),
+                      SizedBox(
+                        width: iconSize / 2,
+                      ),
+                      FlickFullScreenToggle(
+                        size: iconSize,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
